2442b0b84d9746e1d324f40f1f5ece0b9d89fe71
qmj20 <71073521+qmj20@users.noreply.github.com>
2022-03-08 16:11:40 +0800

    Add files via upload

---
+++ seckill-view (2)/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+  <app-header v-if="header_show"></app-header>
+  <router-view v-on:public_header="public_header"></router-view>
+  </div>
+</template>
+
+<script>
+import AppHeader from './components/Header'
+export default {
+  name: 'App',
+  data(){
+    return {
+      header_show:true,
+    }
+  },
+  components: {
+    'app-header':AppHeader,
+  },
+  methods:{
+    //是否显示头部
+    public_header:function (bool) {
+      this.header_show = bool;
+    },
+  }
+}
+</script>
+
+<style>
+
+</style>
